a4a296f80e1bac6ef677dd3a55addcfa37a94de7
Rafał Kmiecik <rafal.kmiecik@racing.agh.edu.pl>
2021-09-14 21:02:57 +0200

    fixed servo_to_PWN structure

--- src/servo_to_PWM.v
+++ src/servo_to_PWM.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.08.2021 20:32:26
-// Design Name: 
-// Module Name: servo_to_PWM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-//TODO
 // updating PWM only every 20ms time interval 
 module servo_to_PWM(
    input wire clk,
@@ -29,49 +9,43 @@ module servo_to_PWM(
    output reg PWM_L,
    output reg PWM_R
    );
-
-reg [19:0] counter =0;
+reg PWM_L_nxt;
+reg PWM_R_nxt;
+reg [19:0] counter,counter_nxt;
 reg [20:0] servo_l_mul,servo_r_mul;
 reg [7:0] servo_L_buf, servo_R_buf;
 
-always @(posedge clk) begin
-   if(rst) begin
-      servo_L_buf <= 0;
-      servo_R_buf <= 0;
-   end
-   else begin
-      servo_L_buf <= servo_L;
-      servo_R_buf <= servo_R;
-   end
-end
+always@ (posedge clk) begin
+  if(rst) begin
+  counter <=0;
+  PWM_L   <=0;
+  PWM_R   <=0;
+  end
+  else begin
+  counter <=counter_nxt;
+  PWM_L   <=PWM_L_nxt;
+  PWM_R   <=PWM_R_nxt;
+  end
+  end 
     
-always @ (posedge clk) begin
-   if(rst) begin
-      servo_l_mul = 0;
-      servo_r_mul = 0;
-      counter = 0;
-      PWM_L = 0;
-      PWM_R = 0;
-   end
-   else begin
-      servo_l_mul =servo_L_buf *1000;
-      servo_r_mul =servo_R_buf *1000;
-      counter = counter+1;
+always @* begin
+      servo_l_mul =servo_L *1000;
+      servo_r_mul =servo_R *1000;
+      counter_nxt = counter+1;
       if(counter > 1000000) begin
-         counter=0;
+         counter_nxt=0;
       end
       if(counter > servo_l_mul) begin
-      PWM_L=0;
+      PWM_L_nxt=0;
       end
       else begin
-      PWM_L =1;
+      PWM_L_nxt =1;
       end
       if(counter >servo_r_mul) begin
-         PWM_R=0;
+         PWM_R_nxt=0;
       end
       else begin
-         PWM_R =1;
+         PWM_R_nxt =1;
       end
-   end
-end  
+   end 
 endmodule
